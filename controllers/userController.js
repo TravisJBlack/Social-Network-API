aed3d8a76393894876eb20bb4d0096ffcfc4c160
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-07-03 22:12:37 -0500

    added deleteFriend and addFriend to userController and updated the routes to accept them

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+    // returns all users
     async getUsers(req, res) {
         try {
             const users = await User.find();
@@ -11,6 +12,7 @@ module.exports = {
         }
     },
 
+    //get one user provided by the req.params
     async getOneUser(req, res) {
         try {
             const oneUser = await User.findOne({ _id: req.params.userId })
@@ -26,6 +28,7 @@ module.exports = {
         }
     },
 
+    //creates a new user
     async createUser(req, res) {
         try {
             const user = await User.create(req.body);
@@ -36,6 +39,7 @@ module.exports = {
         }
     },
 
+    //delets a user via the req.params 
     async deleteUser(req, res) {
         try {
             const user = await User.findOneAndDelete({ _id: req.params.userId })
@@ -51,21 +55,68 @@ module.exports = {
         }
     },
 
-    async updateUser (req, res) {
-        try{
+    //updates the user provided by the req.params variable
+    async updateUser(req, res) {
+        try {
             const user = await User.findOneAndUpdate(
-                {_id: req.params.userId},
+                { _id: req.params.userId },
                 { $set: req.body },
                 { runValidators: true, new: true }
             );
 
             if (!user) {
-                res.status(400).json({message: 'No user with this ID'})
+                res.status(400).json({ message: 'No user with this ID' })
             }
 
             res.json(user);
         } catch (err) {
             res.staus(500).json(err);
         }
-    }
+    },
+
+    // add friend to a User
+    async addFriend(req, res) {
+        try {
+            const friend = await User.findOne({ _id: req.params.friendId });
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: friend._id } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: 'No user with that ID!' });
+            }
+
+            res.json({ message: 'Added friend' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    //removes provided friend from the provided user both via req.params
+    async deleteFriend(req, res) {
+        try {
+            const friend = await User.findOne({ _id: req.params.friendId });
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: friend._id } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: 'No user with that ID!' });
+            }
+
+            res.json({ message: 'Added friend' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
 }
--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 const userData = require('./userData.json');
 const thoughtData = require('./thoughtsData.json');
@@ -9,23 +9,24 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
     console.log('connected');
 
-    let userCheck = await connection.db.listCollections({ name: 'user'}).toArray();
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
     if (userCheck.length) {
-        await connection.dropCollection('user');
+        await connection.dropCollection('users');
     }
 
-    let thoughtCheck = await connection.db.listCollections({ name: 'thought'}).toArray();
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
     if (thoughtCheck.length) {
-        await connection.dropCollection('user');
+        await connection.dropCollection('thoughts');
     }
 })
 
-const seedDB =  async () => {
-
+const seedDB = async () => {
+ 
     const user = await User.create(userData);
+    console.log(user);
 
     const thought = await Thought.create(thoughtData);
-    
+
     process.exit(0);
 }
 
